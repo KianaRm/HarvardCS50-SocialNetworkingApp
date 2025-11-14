75679e717dcb4ffb5d32c77873401668c4e058d3
Kiana Ramzanzadeh <138225536+KianaRm@users.noreply.github.com>
2025-11-14 11:56:20 +0300

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -120,5 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "network" / "static",
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
